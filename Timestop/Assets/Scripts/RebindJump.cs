0f09abd17599a16b0ea603d469aa0ba140c2fdb2
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-08-26 21:54:58 +0100

    attempt nr 2

---
+++ Timestop/Assets/Scripts/RebindJump.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class RebindingDisplay : MonoBehaviour
+{
+    [SerializeField] private InputActionReference jump = null;
+    [SerializeField] private PlayerController playerController = null;
+    [SerializeField] private TMP_Text bindingDispTxt = null;
+    [SerializeField] private GameObject startRebindObj = null;
+    [SerializeField] private GameObject waitingForInputObj = null;
+
+    private InputActionRebindingExtensions.RebindingOperation rebOp1;
+
+    public void StartRebinding()
+    {
+        startRebindObj.SetActive(false);
+        waitingForInputObj.SetActive(true);
+
+        playerController.PlayerInput.SwitchCurrentActionMap("Menu");
+
+        rebOp1 = jump.action.PerformInteractiveRebinding().WithControlsExcluding("Mouse").OnMatchWaitForAnother(0.1f).OnComplete(operation => RebindComplete()).Start();
+
+    }
+
+    private void RebindComplete()
+    {
+        rebOp1.Dispose();
+
+        startRebindObj.SetActive(true);
+        waitingForInputObj.SetActive(false);
+
+        playerController.PlayerInput.SwitchCurrentActionMap("Menu");
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
