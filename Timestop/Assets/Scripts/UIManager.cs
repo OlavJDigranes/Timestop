64c1af88439bf2d68ee881f10232823bef70ddd6
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-09-01 12:02:03 +0100

    Update UIManager.cs
    Minor comments addition

--- Timestop/Assets/Scripts/UIManager.cs
+++ Timestop/Assets/Scripts/UIManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //Usie this to display subtitles/level information?
+
     void Start()
     {
         
