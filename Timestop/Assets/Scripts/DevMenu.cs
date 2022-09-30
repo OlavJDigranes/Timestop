004b76171e1ecb79406fb7fd2e0c9a7809d1e79f
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-09-30 17:09:47 +0100

    Added dev menu for level selection

---
+++ Timestop/Assets/Scripts/DevMenu.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DevMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Level1(){
+        SceneManager.LoadScene(1, LoadSceneMode.Single); 
+    }
+
+    public void Level2(){
+        SceneManager.LoadScene(2, LoadSceneMode.Single); 
+    }
+
+    public void Level3(){
+        SceneManager.LoadScene(3, LoadSceneMode.Single); 
+    }
+
+    public void Level4(){
+        SceneManager.LoadScene(4, LoadSceneMode.Single); 
+    }
+
+    public void Level5(){
+        SceneManager.LoadScene(5, LoadSceneMode.Single); 
+    }
+
+    public void Level6(){
+        SceneManager.LoadScene(6, LoadSceneMode.Single); 
+    }
+
+    public void Level7(){
+        SceneManager.LoadScene(7, LoadSceneMode.Single); 
+    }
+
+    public void Level8(){
+        SceneManager.LoadScene(8, LoadSceneMode.Single); 
+    }
+
+    public void Level9(){
+        SceneManager.LoadScene(9, LoadSceneMode.Single); 
+    }
+
+    public void Level10(){
+        SceneManager.LoadScene(10, LoadSceneMode.Single); 
+    }
+
+    public void Level11(){
+        SceneManager.LoadScene(11, LoadSceneMode.Single); 
+    }
+
+    public void Level12(){
+        SceneManager.LoadScene(12, LoadSceneMode.Single); 
+    }
+
+    public void Level13(){
+        SceneManager.LoadScene(13, LoadSceneMode.Single); 
+    }
+
+    public void Level14(){
+        SceneManager.LoadScene(14, LoadSceneMode.Single); 
+    }
+
+    public void Level15(){
+        SceneManager.LoadScene(15, LoadSceneMode.Single); 
+    }
+
+    public void Level16(){
+        SceneManager.LoadScene(16, LoadSceneMode.Single); 
+    }
+
+    public void Level17(){
+        SceneManager.LoadScene(17, LoadSceneMode.Single); 
+    }
+
+    public void Level18(){
+        SceneManager.LoadScene(18, LoadSceneMode.Single); 
+    }
+
+    public void Level19(){
+        SceneManager.LoadScene(19, LoadSceneMode.Single); 
+    }
+
+    public void Level20(){
+        SceneManager.LoadScene(20, LoadSceneMode.Single); 
+    }
+
+    public void Level21(){
+        SceneManager.LoadScene(21, LoadSceneMode.Single); 
+    }
+
+    public void Level22(){
+        SceneManager.LoadScene(22, LoadSceneMode.Single); 
+    }
+
+    public void Level23(){
+        SceneManager.LoadScene(23, LoadSceneMode.Single); 
+    }
+
+    public void Level77(){
+        SceneManager.LoadScene("Level_77", LoadSceneMode.Single); 
+    }
+}
